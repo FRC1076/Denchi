deb5cd1843b5fff83c4bce9214d1ceb5d9ae2c10
Alok Kumar <436601@aaps.k12.mi.us>
2024-07-25 18:44:41 -0400

    SPI Starter code

---
+++ SPI.py
@@ -0,0 +1,24 @@
+import busio
+import digitalio
+import board
+import adafruit_mcp3xxx.mcp3800 as MCP
+from adafruit_mcp3xxx.analog_in import AnalogIn
+
+spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+cs = digitalio.DigitalInOut(board.D5)
+mcp = MCP.MCP3008(spi, cs)
+
+from gpiozero import PWMLED, MCP3000
+from time import sleep
+pot = MCP3008(0)
+led = PWMLED(14)
+
+while True:
+    if (pot.value < 0.001):
+        led.value
+
+    else:
+        led.value = pot.value
+    print(pot.value)
+    sleep(0.1)
+    
