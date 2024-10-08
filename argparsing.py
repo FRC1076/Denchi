8d403352696efb1b84049da65fdba36b85ad7050
Jesse Kane <jessekane66@gmail.com>
2024-10-08 14:53:27 -0400

    added battery testing

--- argparsing.py
+++ argparsing.py
@@ -2,6 +2,9 @@ import argparse
 import time
 import sys
 import datetime
+import testADC as ADC
+
+battery = ADC.fake0MPC3008()
 parser = argparse.ArgumentParser(
     description='criteria for battery conditioner file')
 
@@ -31,18 +34,22 @@ parser.add_argument('--id', type=str,
                     help= 'BatteryID')
 parser.add_argument('--team', type=str,
                     help= 'TeamID', nargs='?',const='1076',default='1076')
-parser.add_argument('--loadohms', type=str,
+parser.add_argument('--loadohms', type=float,
                     help= 'LoadOhms')
 parser.add_argument('--outfile', type=str,
                     help= 'OutputFile', nargs='?', const='history.dat', default='history.dat')
+parser.add_argument('--polltime', type=int,
+                    help='Polling interval in milliseconds', nargs='?',const='100',default='100')
 
 #args.log.write('%s' % (args.id, args.team, args.loadohms, args.outfile))
 #args.log.close()
 
 args = parser.parse_args()
-
 file = open(args.outfile, 'a') 
 file.write("------------------------------------------------\n")
 file.write("# batcon Battery Conditioner and Capacity Test")
-file.write(f"\n# TeamID: {args.team} \n# BatteryID: {args.id} \n# LoadOhms: {args.loadohms} \n# StartTime: {str(datetime.datetime.now().astimezone())}\n")
+file.write(f"\n# TeamID: {args.team} \n# BatteryID: {args.id} \n# LoadOhms: {str(args.loadohms)} \n# StartTime: {str(datetime.datetime.now().astimezone())}\n# PollTime: {str(args.polltime)}\n")
+for i in range(30):
+    file.write(f'# TEST {i}: VOLTS={battery.voltage()/1000},CURRENT={battery.voltage()/(1000 * args.loadohms)},TIME={str(datetime.datetime.now().time())}\n')
+    time.sleep(args.polltime/1000)
 file.close()
