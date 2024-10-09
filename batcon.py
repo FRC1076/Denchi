8d786214cb3044054ea186fefc291c0f65efb2af
Jesse Kane <jessekane66@gmail.com>
2024-10-08 20:58:57 -0400

    integrator, removed pollnum

--- batcon.py
+++ batcon.py
@@ -1,10 +1,9 @@
 import argparse
 import time
-import sys
 import datetime
 import testADC as ADC
 import hashlib
-import random
+import scipy.integrate as integrate
 
 battery = ADC.fake0MPC3008([12000,11500,11000,10500,10000])
 parser = argparse.ArgumentParser(
@@ -51,22 +50,36 @@ if not args.id:
     raise RuntimeError("no battery ID specified")
 elif not args.loadohms:
     raise RuntimeError("no resistance load specified")
-timestamp = str(datetime.datetime.now().astimezone())
+initialTimestamp = datetime.datetime.now().astimezone()
+timestamp = str(initialTimestamp)
 hashSeedString = str(args.team)+str(args.id)+str(args.loadohms)+str(args.polltime)+timestamp
 hashSeedBytes = bytearray()
 hashSeedBytes.extend(map(ord, hashSeedString))
 hasher = hashlib.shake_256(hashSeedBytes)
+print("batcon Battery Conditioner and Capacity Test")
+print(f"HashID: {hasher.hexdigest(4)}\nTeamID: {args.team}\nBatteryID: {args.id}\nLoadOhms: {str(args.loadohms)}\nStartTime: {timestamp}\nPollTime: {str(args.polltime)}\n")
 
 file = open(args.outfile, 'a') 
 file.write("------------------------------------------------\n")
 file.write("# batcon Battery Conditioner and Capacity Test")
 file.write(f"\n# HashID: {hasher.hexdigest(4)}\n# TeamID: {args.team}\n# BatteryID: {args.id}\n# LoadOhms: {str(args.loadohms)}\n# StartTime: {timestamp}\n# PollTime: {str(args.polltime)}\nVoltage,Current,Timestamp\n")
-readings = []
+currentReadings = [] #list of current readings (amperes)
+timedeltas = [] #list of time deltas where readings were taken (seconds)
+
 for i in range(5):
-    timestamp = datetime.datetime.now().time() #timestamp
+    timestamp = datetime.datetime.now().astimezone() #timestamp
     voltage = battery.voltage()/1000 #voltage in volts
     current = voltage/args.loadohms #current in amps
     file.write(f'{str(voltage)},{str(current)},{str(timestamp)}\n')
-    readings.append((voltage,current,timestamp))
+    currentReadings.append(current)
+    timedeltas.append((timestamp-initialTimestamp).total_seconds())
     time.sleep(args.polltime/1000)
+
+ampereHours = integrate.simpson(
+    y = currentReadings,
+    x = timedeltas
+) # Gives result in ampere-seconds, not ampere hours. units need to be converted
+ampereHours *= 0.0002777778
+file.write(f'Battery Life: {str(ampereHours)}\n')
 file.close()
+
--- batconReader.py
+++ batconReader.py
@@ -12,11 +12,13 @@ class batteryTest:
     pollTime : int
     timeStart : datetime
     readings : list[tuple[float,float,datetime]]
+    batteryLife : float
 
     def __str__(self):
         outStr = f'# batcon Battery Conditioner and Capacity Test\n# HashID: {self.hashID}\n# TeamID: {self.teamID}\n# BatteryID: {self.batteryID}\n# LoadOhms: {str(self.loadOhms)}\n# StartTime: {str(self.timeStart)}\n# PollTime: {str(self.pollTime)}\nVoltage,Current,Timestamp\n'
         for reading in self.readings:
             outStr += f'{str(reading[0])},{str(reading[1])},{str(reading[2])}\n'
+        outStr += f'Battery Life: {self.batteryLife}\n'
         return outStr
     
 class batconReader:
@@ -42,10 +44,11 @@ class batconReader:
                 starttime = datetime.strptime(header.group(5), '%Y-%m-%d %H:%M:%S.%f%z')
                 polltime = int(header.group(6))
                 readings = entry.split('Voltage,Current,Timestamp\n')[1]
-                readings = readings.split('\n')[:-1]
+                readings = readings.split('\n')[:-1] #Removes trailing newline
+                result = readings.pop(-1)[14:] #removes battery life result string from the readings. the first 14 characters are 'Battery Life: '
                 readings = csv.reader(readings)
-                processedReadings = [(float(reading[0]),float(reading[1]),datetime.strptime(reading[2], '%H:%M:%S.%f').time()) for reading in readings]
-                self.tests[hashid] = batteryTest(hashid,teamid,batid,loadohms,polltime,starttime,processedReadings)
+                processedReadings = [(float(reading[0]),float(reading[1]),datetime.strptime(reading[2], '%Y-%m-%d %H:%M:%S.%f%z')) for reading in readings]
+                self.tests[hashid] = batteryTest(hashid,teamid,batid,loadohms,polltime,starttime,processedReadings,float(result))
     
 if __name__ == '__main__':
     reader = batconReader('history.dat')
