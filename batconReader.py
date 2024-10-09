1ccba67b799c490fb10743f0f0f28cc93bd5a75b
Jesse Kane <jessekane66@gmail.com>
2024-10-09 12:36:09 -0400

    stuff

--- batconReader.py
+++ batconReader.py
@@ -5,7 +5,7 @@ import csv
 
 @dataclass
 class batteryTest:
-    hashID : str
+    fingerprint : str
     teamID : str
     batteryID : str
     loadOhms : float
@@ -15,14 +15,14 @@ class batteryTest:
     batteryLife : float
 
     def __str__(self):
-        outStr = f'# batcon Battery Conditioner and Capacity Test\n# HashID: {self.hashID}\n# TeamID: {self.teamID}\n# BatteryID: {self.batteryID}\n# LoadOhms: {str(self.loadOhms)}\n# StartTime: {str(self.timeStart)}\n# PollTime: {str(self.pollTime)}\nVoltage,Current,Timestamp\n'
+        outStr = f'# batcon Battery Conditioner and Capacity Test\n# Fingerprint: {self.fingerprint}\n# TeamID: {self.teamID}\n# BatteryID: {self.batteryID}\n# LoadOhms: {str(self.loadOhms)}\n# StartTime: {str(self.timeStart)}\n# PollTime: {str(self.pollTime)}\nVoltage,Current,Timestamp\n'
         for reading in self.readings:
             outStr += f'{str(reading[0])},{str(reading[1])},{str(reading[2])}\n'
         outStr += f'Battery Life: {self.batteryLife}\n'
         return outStr
     
 class batconReader:
-    headerRegex = re.compile(r'# batcon Battery Conditioner and Capacity Test\n# HashID: (.+)\n# TeamID: (.+)\n# BatteryID: (.+)\n# LoadOhms: (.+)\n# StartTime: (.+)\n# PollTime: (.+)')
+    headerRegex = re.compile(r'# batcon Battery Conditioner and Capacity Test\n# Fingerprint: (.+)\n# TeamID: (.+)\n# BatteryID: (.+)\n# LoadOhms: (.+)\n# StartTime: (.+)\n# PollTime: (.+)')
     def __init__(self,filepath=None):
         self.filepath = filepath
         self.tests = {}
@@ -37,7 +37,7 @@ class batconReader:
         for entry in entryList:
             header = self.headerRegex.match(entry)
             if header:
-                hashid = header.group(1)
+                fprint = header.group(1)
                 teamid = header.group(2)
                 batid = header.group(3)
                 loadohms = float(header.group(4))
@@ -48,7 +48,7 @@ class batconReader:
                 result = readings.pop(-1)[14:] #removes battery life result string from the readings. the first 14 characters are 'Battery Life: '
                 readings = csv.reader(readings)
                 processedReadings = [(float(reading[0]),float(reading[1]),datetime.strptime(reading[2], '%Y-%m-%d %H:%M:%S.%f%z')) for reading in readings]
-                self.tests[hashid] = batteryTest(hashid,teamid,batid,loadohms,polltime,starttime,processedReadings,float(result))
+                self.tests[fprint] = batteryTest(fprint,teamid,batid,loadohms,polltime,starttime,processedReadings,float(result))
     
 if __name__ == '__main__':
     reader = batconReader('history.dat')
